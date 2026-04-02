3f719459bd692a66cfd950cbe6f8039d1fba78cb
zhangyue616 <68990166+zhangyue616@users.noreply.github.com>
2026-04-03 05:44:53 +0800

    Delete kano_placeholder directory

--- kano_placeholder/__init__.py
+++
@@ -1,3 +0,0 @@
-# OWL2Vec-Star placeholder
-def get_owl2vec_embeddings(*args, **kwargs):
-    raise NotImplementedError("OWL2Vec-Star not installed. Install with: pip install git+https://github.com/KRR-Oxford/OWL2Vec-Star.git")
